3c095393b37c26b8f5fe397d55c249a3dbd8cfaf
[IoT3] 조수빈 <cdw3521@naver.com>
2024-04-30 18:05:13 +0900

    🐥 같은 숫자는 싫어

---
+++ programers/Lv.1/12906/12906.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list=new ArrayList<>();
+		list.add(arr[0]);
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]!=arr[i-1]) {
+				list.add(arr[i]);
+			}
+		}
+		return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
