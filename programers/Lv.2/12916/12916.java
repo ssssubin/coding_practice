20c65170b8ad36851201d4a264eba8e12314db6b
subin <cdw3521@naver.com>
2024-05-29 15:15:33 +0900

    😺 멀리 뛰기

---
+++ programers/Lv.2/12916/12916.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long solution(int n) {
+        
+        long[] answer= new long[2001];
+        answer[1]=1;
+        answer[2]=2;
+        for(int i=3; i<=n; i++){
+            answer[i]=(answer[i-1]+answer[i-2])%1234567;
+        }
+        return answer[n];
+    }
+}
