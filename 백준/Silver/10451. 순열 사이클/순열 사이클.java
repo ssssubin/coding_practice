79d6ff3dbd67e657985c2dd36cd33db932343129
ssssubin <122021027+ssssubin@users.noreply.github.com>
2024-07-18 00:22:36 +0900

    [Silver III] Title: 순열 사이클, Time: 764 ms, Memory: 63564 KB -BaekjoonHub

---
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] graph;
+    static boolean[] visited;
+
+    public static void dfs(int index){
+        visited[index] = true;
+        if(visited[graph[index]] == false){
+            dfs(graph[index]);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t< T; t++){
+            int N = Integer.parseInt(br.readLine());
+            int[] input = new int[N], inputSort = new int[N];
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            graph = new int[N+1];
+            visited = new boolean[N+1];
+
+            for(int i=0; i<N; i++){
+                input[i] = Integer.parseInt(st.nextToken());
+                inputSort[i]= input[i];
+            }
+            Arrays.sort(inputSort);
+            for(int i=0; i<N; i++){
+                int x = inputSort[i];
+                int y = input[i];
+                graph[x] = y;
+            }
+
+            int answer = 0;
+            for (int i : input) {
+                if(visited[i] == false){
+                    dfs(i);
+                    answer++;
+                }
+            }
+            System.out.println(answer);
+        }
+    }
+}
